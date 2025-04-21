03e022974c4be024a63b1c1743b40d7796065e67
Maksim Kezin <kezin.maks@gmail.com>
2025-04-21 21:52:12 +0300

    MSM-22 Refactor pkg/vault. Separate func create Vault client from creating vault.Options

--- pkg/vault/vault.go
+++ pkg/vault/vault.go
@@ -17,16 +17,11 @@ type Options struct {
 }
 
 func New(cfg config.Vault) (Options, error) {
-	client, err := vault.New(
-		vault.WithAddress(fmt.Sprintf("%s:%s", cfg.Address, cfg.Port)),
-		vault.WithRequestTimeout(requestTimeout),
-	)
+	host := fmt.Sprintf("%s:%s", cfg.Address, cfg.Port)
+
+	client, err := create(host, cfg.Token)
 
 	if err != nil {
-		return Options{}, err
-	}
-
-	if err := client.SetToken(cfg.Token); err != nil {
 		return Options{}, err
 	}
 
@@ -34,4 +29,21 @@ func New(cfg config.Vault) (Options, error) {
 		cfg,
 		client,
 	}, err
+}
+
+func create(host string, token string) (*vault.Client, error) {
+	client, err := vault.New(
+		vault.WithAddress(host),
+		vault.WithRequestTimeout(requestTimeout),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.SetToken(token); err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
